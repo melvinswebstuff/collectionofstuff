22bbdb7024fa8352a2223be95957d1cfabaf75df
Melvin <me@melvinswebstuff.com>
2016-04-18 18:12:47 -0400

    Add more stuff based on c# threading youtube video Jamie King

--- AsyncAwait/Program.cs
+++ AsyncAwait/Program.cs
@@ -1,15 +1,126 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncAwait
 {
     class Program
     {
+        static byte[] values = new byte[500000000];
+        static long[] portionResults;
+        static int portionSize;
+        static int processorCnt = Environment.ProcessorCount;
+        static MySchronizedQueue<int> numbers = new MySchronizedQueue<int>();
+        const int numThreads = 3;
+        static int[] sums = new int[numThreads];
+
         static void Main(string[] args)
         {
-            StartSomethingAsync();
+            portionResults = new long[processorCnt];
+            portionSize = values.Length / processorCnt;
+            //StartSomethingAsync();
+            //GenerateInts();
+            ProduceSumNumbers();
             Console.Read();
+        }
+
+        private static void ProduceSumNumbers()
+        {
+            var producingThread = new Thread(ProduceNumbers);
+            producingThread.Start();
+            Thread[] threads = new Thread[numThreads];
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                threads[i] = new Thread(SumNumbers);
+                threads[i].Start(i);
+            }
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                threads[i].Join();
+            }
+
+            int totalSum = 0;
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                totalSum += sums[i];
+            }
+
+            Console.WriteLine("Done adding. Total is " + totalSum);
+        }
+
+        static void SumYourPortion(object portionNumber)
+        {
+            long sum = 0;
+            int portionNumberAsInt = (int)portionNumber;
+            int baseIndex = portionNumberAsInt * portionSize;
+
+            for (int i = baseIndex; i < baseIndex + portionSize; i++)
+            {
+                sum += values[i];
+            }
+
+            portionResults[portionNumberAsInt] = sum;
+        }
+
+        private static void GenerateInts()
+        {
+            Stopwatch sw = new Stopwatch();
+            Random rand = new Random(987);
+            long total1 = 0;
+
+            sw.Start();
+
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = (byte)rand.Next(10);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("Random numbers generated..." + sw.Elapsed.TotalSeconds);
+
+            sw.Reset();
+            sw.Start();
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                total1 += values[i];
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(total1 + "...Sum of random numbers..." + sw.Elapsed);
+
+            sw.Reset();
+            sw.Start();
+
+            Thread[] threads = new Thread[processorCnt];
+
+            for (int i = 0; i < processorCnt; i++)
+            {
+                threads[i] = new Thread(SumYourPortion);
+                threads[i].Start(i);
+            }
+
+            for (int i = 0; i < processorCnt; i++)
+            {
+                threads[i].Join();
+            }
+            long total2 = 0;
+            for (int i = 0; i < processorCnt; i++)
+            {
+                total2 += portionResults[i];
+            }
+
+            sw.Stop();
+            Console.WriteLine(total2 + "...Sum of random numbers..." + sw.Elapsed);
         }
 
         private static async void StartSomethingAsync()
@@ -26,12 +137,12 @@ namespace AsyncAwait
                         // Note that you could put "await ExampleMethodAsync()" in the next line where
                         // "length" is, but due to when '+=' fetches the value of ResultsTextBox, you
                         // would not see the global side effect of ExampleMethodAsync setting the text.
-                        Console.WriteLine(String.Format("Length: {0}", length));
+                        Console.WriteLine("Length: {0}" + length);
                     }
                 }
 
-                
-                
+
+
             }
             catch (Exception)
             {
@@ -69,5 +180,79 @@ namespace AsyncAwait
                 Console.WriteLine(i);
             }
         }
+
+        static void ProduceNumbers()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Random rand = new Random(987);
+            for (int i = 0; i < 10; i++)
+            {
+                int numToEnqueue = rand.Next(10);
+                lock (numbers)
+                {
+                    numbers.Enqueue(numToEnqueue);
+                }
+                Console.WriteLine("Producing thread adding " + numToEnqueue + " to the queue");
+                Thread.Sleep(rand.Next(1000));
+            }
+            sw.Stop();
+            Console.WriteLine("ProducedNumbers: " + sw.Elapsed);
+        }
+
+        static void SumNumbers(object threadNumber)
+        {
+            DateTime startTime = DateTime.Now;
+            int mySum = 0;
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            while ((DateTime.Now - startTime).Seconds < 11)
+            {
+                int numToSum = -1;
+                lock (numbers.SyncRoot)
+                {
+                    if (numbers.Count != 0)
+                    {
+                        numToSum = numbers.Dequeue();
+                    }
+                }
+
+                if (numToSum != -1)
+                {
+                    mySum += numToSum;
+                    Console.WriteLine("Consuming thread #" + threadNumber + " adding " + numToSum + " to its total sum making " + mySum + " for the thread total.");
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine("SumNumbers on thread # " + threadNumber + " took " + sw.Elapsed);
+
+            sums[(int)threadNumber] = mySum;
+        }
+    }
+}
+
+class MySchronizedQueue<T>
+{
+    object baton = new object();
+    Queue<T> theQ = new Queue<T>();
+    public void Enqueue(T item)
+    {
+
+        lock(baton) theQ.Enqueue(item);
+    }
+    public T Dequeue()
+    {
+        lock (baton)  return theQ.Dequeue();
+    }
+    public int Count
+    {
+        get { lock (baton)  return theQ.Count; }
+    }
+    public object SyncRoot
+    {
+        get { return baton; }
     }
 }
---
+++ PublicRestroom/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+namespace PublicRestroom
+{
+    class BathroomStall
+    {
+        object baton = new object();
+        public void BeUsed()
+        {
+            lock (baton)
+            {
+                Console.WriteLine("Doing our thing...");
+
+            }
+        }
+
+        public void FlushToilet()
+        {
+            lock (baton)
+            {
+                Console.WriteLine("Flushing the toilet...");
+            }
+        }
+    }
+    class PublicRestroom
+    {
+        object batonStall1 = new object();
+        object batonStall2 = new object();
+        object batonSink1 = new object();
+        object batonSink2 = new object();
+        public void UseStall1()
+        {
+            lock (batonStall1)
+            {
+                Console.WriteLine("In stall 1 ");
+                Thread.Sleep(2000);
+            }
+        }
+        public void UseStall2()
+        {
+            lock (batonStall2)
+            {
+                Console.WriteLine("In stall 2 ");
+                Thread.Sleep(2000);
+            }
+        }
+        public void UseSink1()
+        {
+            lock (batonSink1)
+            {
+                Console.WriteLine("In sink 1 ");
+                Thread.Sleep(2000);
+            }
+        }
+        public void UseSink2()
+        {
+            lock (batonSink2)
+            {
+                Console.WriteLine("In sink 2 ");
+                Thread.Sleep(2000);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PublicRestroom pr = new PublicRestroom();
+            new Thread(pr.UseStall1).Start();
+            new Thread(pr.UseStall2).Start();
+            new Thread(pr.UseSink1).Start();
+            new Thread(pr.UseSink2).Start();
+            Console.Read();
+        }
+    }
+}
